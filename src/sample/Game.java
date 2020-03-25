809b483994b9f99fc84f8116e44d7a1563a61ad5
Pieter Beens <beens.pieter@gmail.com>
2020-03-25 14:55:59 +0100

    FF updaten

--- src/sample/Game.java
+++ src/sample/Game.java
@@ -2,39 +2,27 @@ package sample;
 
 import javafx.application.Application;
 import javafx.application.Platform;
-import javafx.collections.ObservableList;
-import javafx.fxml.FXMLLoader;
-import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.geometry.Rectangle2D;
-import javafx.scene.Node;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
-import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
-import javafx.scene.media.AudioClip;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
 import javafx.scene.paint.Color;
-import javafx.scene.shape.Circle;
-import javafx.scene.shape.Rectangle;
 import javafx.scene.text.Font;
 import javafx.scene.text.TextAlignment;
 import javafx.stage.Screen;
 import javafx.stage.Stage;
 
 import java.io.File;
-import java.lang.reflect.Array;
-import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.Stack;
 
@@ -94,8 +82,8 @@ public class Game extends Application { //TODO: port to Android: https://stackov
 
         HBox titleRow = new HBox(); // <---------------------------------------------------------------------------
 
-        titleRow.getChildren().add(new ImageView(new Image("logo.png")));
-        titleRow.setAlignment(Pos.CENTER);
+  //      titleRow.getChildren().add(new ImageView(new Image("logo.png")));
+  //      titleRow.setAlignment(Pos.CENTER);
 
         Label boardsizeLabel = new Label("CHOOSE NUMBER OF FIELDS");
 
@@ -620,6 +608,8 @@ public class Game extends Application { //TODO: port to Android: https://stackov
             }
         }
 
+        //TODO: known bug: sometimes after a capture the background color is not changed, and can never be changed again?
+
         for (ArrayList<Field> row : this.fields) {
             for (Field field : row) {
                 //ImageView iv = new ImageView(this.x);
@@ -635,7 +625,7 @@ public class Game extends Application { //TODO: port to Android: https://stackov
     }
 
     public void playSound(int i) {
-        //TODO: play .wav and kill thread immediately after
+        //TODO: fix issue where sometimes sound effects fail to play
 
         Media sound = new Media(new File(i%4 + ".wav").toURI().toString());
         MediaPlayer mediaPlayer = new MediaPlayer(sound);
