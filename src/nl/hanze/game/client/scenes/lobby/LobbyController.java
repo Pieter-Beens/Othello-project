5c6619001883efb28c8544b1c7092cf484972db1
Roy Voetman <royvoetman@outlook.com>
2020-04-02 10:13:51 +0200

    Added defaults to input fields in online menu.

--- src/nl/hanze/game/client/scenes/lobby/LobbyController.java
+++ src/nl/hanze/game/client/scenes/lobby/LobbyController.java
@@ -2,16 +2,30 @@ package nl.hanze.game.client.scenes.lobby;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import nl.hanze.game.client.Main;
 import nl.hanze.game.client.scenes.Controller;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
-public class LobbyController extends Controller {
+public class LobbyController extends Controller implements Initializable {
     @FXML
     private void btnLogout(ActionEvent event) throws IOException {
         Main.client.logout();
 
         goBack();
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        Main.client.getGameList();
+        Main.client.getPlayerList();
+    }
+
+    @Override
+    public void update(String response) {
+        super.update(response);
     }
 }
