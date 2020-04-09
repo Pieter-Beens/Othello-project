03aad3f192cc76c09c2be60e48c8e670bc69f4ba
Nicky_de_Scholman <p.nick_scholma@hotmail.com>
2020-04-09 19:07:44 +0200

    Edited the OthelloAiHard to make Julius less retarded

--- src/nl/hanze/game/client/players/AI/OthelloAIHard.java
+++ src/nl/hanze/game/client/players/AI/OthelloAIHard.java
@@ -3,10 +3,12 @@ package nl.hanze.game.client.players.AI;
 import nl.hanze.game.client.players.AI.utils.Move;
 import nl.hanze.game.client.players.Player;
 import nl.hanze.game.client.scenes.games.utils.Field;
+import nl.hanze.game.client.scenes.games.othello.OthelloModel;
 import nl.hanze.game.client.scenes.games.GameModel;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Stack;
 
 /**
  * This AI uses primitive min-maxing to make at least some intelligent decisions.
@@ -22,7 +24,9 @@ public class OthelloAIHard implements AIStrategy {
     private static final int SCORE = 0;
     private static final int ROW = 1;
     private static final int COLUMN = 2;
+
     private static final int MAXDEPTH = 5;
+
     private static final int CORNERSCORE = 20;
     private static final int BORDERSCORE = 5;
     private static final int XCROSSSCORE = -20;
@@ -63,15 +67,16 @@ public class OthelloAIHard implements AIStrategy {
             }
         }
 
-        int[] move = minMax(board,0,true);
-        return new Move(aiPlayer,move[ROW], move[COLUMN]);
+        int[] move = minMax(board,0, player, opponent);
+        return new Move(player ,move[ROW], move[COLUMN]);
     }
 
-    private int[] minMax(Field[][] board, int depth, boolean isItJulius) {
+    private int[] minMax(Field[][] board, int depth, Player player, Player opponent) {
         int score;
-        int bestScore;
+        int bestScore = 0;
         int bestRow = -1;
         int bestCol = -1;
+        Field[][] boardCopy;
 
         if (depth >= MAXDEPTH) {
             score = scoreMap.get(lastMove);
@@ -87,35 +92,83 @@ public class OthelloAIHard implements AIStrategy {
             }
         }
 
-        if (isItJulius) { //ai is playing
-            bestScore = -500000;
-            for (Field cell : validMoves) {
-                cell.setOwner(aiPlayer);
-                lastMove = cell;
-                score = minMax(board, depth + 1, false)[SCORE];
-                cell.setOwner(null);
+        for (Field cell : validMoves) {
+            cell.setOwner(player);
+            boardCopy = enactCaptures(cell, board, player, opponent);
+            lastMove = cell;
+            score = minMax(boardCopy, depth + 1, opponent, player)[SCORE];
+            cell.setOwner(null);
 
-                if (score > bestScore) {
-                    bestScore = score;
-                    bestRow = cell.getRowID();
-                    bestCol = cell.getColumnID();
-                }
-            }
-        } else { //human is playing
-            bestScore = 500000;
-            for (Field cell : validMoves) {
-                cell.setOwner(humanPlayer);
-                score = minMax(board, depth + 1, true)[SCORE] * -1;
-                cell.setOwner(null);
-
-                if (score < bestScore) {
-                    bestScore = score;
+            if (score > bestScore) {
+                bestScore = score;
+                if (depth == 0) {
                     bestRow = cell.getRowID();
                     bestCol = cell.getColumnID();
                 }
             }
         }
+
         return new int[]{bestScore, bestRow, bestCol};
+    }
+
+    private Field[][] enactCaptures(Field field, Field[][] board, Player player, Player opponent) {
+        Field[][] boardCopy = board.clone();
+        Stack<Field> captures = getCaptures(field, boardCopy, player, opponent);
+        for (Field capturedField : captures) {
+            capturedField.setOwner(player);
+
+        }
+        return boardCopy;
+    }
+
+    private Stack<Field> getCaptures(Field field, Field[][] board, Player player, Player opponent) {
+        Stack<Field> allCaptures = new Stack<>();
+
+        // occupied Fields are never a valid move - immediately return empty Stack
+        if (field.getOwner() != null) {
+            return allCaptures;
+        }
+
+        for (int[] direction : GameModel.DIRECTIONS) {
+
+            Stack<Field> capturesInThisDirection = new Stack<>();
+
+            int currentRowID = field.getRowID();
+            int currentColumnID = field.getColumnID();
+
+            while (true) {
+                int nextRowID = currentRowID + direction[0];
+                int nextColumnID = currentColumnID + direction[1];
+
+                // start with all cases where a direction will never make a move valid, so no further checks are necessary
+                // directly borders a place outside the board > NO CAPTURES HERE
+                if (nextRowID >= board.length || nextColumnID >= board.length || nextRowID < 0 || nextColumnID < 0) break;
+                Field nextField = board[nextRowID][nextColumnID];
+
+                // directly borders an empty Field > NO CAPTURES HERE
+                if (nextField.getOwner() == null) break;
+                // directly borders a Field with the same owner > NO CAPTURES HERE
+                if (nextField.getOwner() == player && capturesInThisDirection.size() == 0) break;
+
+                // success condition: if the loop has looped at least once and the next Field has the same owner, the captures are added to allCaptures
+                if (nextField.getOwner() == player) {
+                    while (!capturesInThisDirection.isEmpty()) {
+                        allCaptures.push(capturesInThisDirection.pop());
+                    }
+                    break;
+                }
+
+                // if the next Field has another owner, this might be a capture!
+                if (nextField.getOwner() == opponent) {
+                    currentRowID = nextRowID;
+                    currentColumnID = nextColumnID;
+                    capturesInThisDirection.push(nextField);
+                }
+                // if the loop got this far, it means there is potential for a capture; the next iteration will check one Field further...
+            }
+        }
+        // returns captures in all directions
+        return allCaptures;
     }
 
 }
